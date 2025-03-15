7809f88bc49e3fea37b579737e961786011d651f
Mayank Joshi <mayank.joshi@chekk.me>
2025-03-15 10:52:16 +0530

    trying to make the api call better v7

--- app/backend/projects/page.tsx
+++ app/backend/projects/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import prisma from "@/app/prisma-db";
 import ProjectTable from "./components/ProjectTable";
 import AddProjectButton from "./components/AddProjectButton";
